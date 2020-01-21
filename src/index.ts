b696a63b9753a91cab326987222f586681b3c159
haesooKr <gotn676@gmail.com>
2020-01-21 00:23:17 -0500

    Creating a block

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
-const sayHi = (name:string, age:number, gender:string):void => {
-  console.log(`Hello ${name}, you are ${age} and ${gender}.`)
+class Block {
+  public index:number;
+  public hash:string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index:number,
+    hash:string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ){
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
 }
 
-sayHi("Haesoo", 27, 'M');
+const genesisBlock:Block = new Block(0, "20202020", "", "Hello", 123456);
 
+let blockchain: [Block] = [genesisBlock];
+
+console.log(blockchain);
 
 export {};
