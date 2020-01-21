c5e015c30cdc724bc314e1a3bccad46725be6a51
haesooKr <gotn676@gmail.com>
2020-01-21 16:11:49 -0500

    Creating a block part two

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,27 @@
+import * as CryptoJS from 'crypto-js';
+
 class Block {
-  public index:number;
-  public hash:string;
+  public index: number;
+  public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
   constructor(
-    index:number,
-    hash:string,
+    index: number,
+    hash: string,
     previousHash: string,
     data: string,
     timestamp: number
-  ){
+  ) {
     this.index = index;
     this.hash = hash;
     this.previousHash = previousHash;
@@ -19,10 +30,36 @@ class Block {
   }
 }
 
-const genesisBlock:Block = new Block(0, "20202020", "", "Hello", 123456);
+const genesisBlock: Block = new Block(0, "20202020", "", "Hello", 123456);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain);
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimestamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimestamp
+  );
+  return newBlock;
+};
+
+console.log(createNewBlock("hello"));
 
 export {};
